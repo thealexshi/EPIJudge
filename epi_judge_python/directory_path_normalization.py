937af0c7df84e4d5f3495219d92aad4245ff1ce1
Alex Shi <thealexshi@gmail.com>
2023-10-24 14:50:23 -0700

    Try 1 at directory path

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,28 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    """
+    ./../ -> ../
+    .././ -> ../ -> if the stack is empty, push .. onto it. We cancel each .. out with every item on the stack
+
+    1. Absolute paths (start with /)
+    2. Relative paths
+
+    """
+    stack = []
+    is_absolute = path[0] == '/'
+    for token in path.split('/'):
+        if token == '' or token == '.':
+            continue
+        elif token == '..':
+            if stack and stack[-1] != '..':
+                stack.pop()
+            else:
+                stack.append(token)
+        else:
+            stack.append(token)
+
+    return '/' + '/'.join(stack) if is_absolute else '/'.join(stack)
 
 
 if __name__ == '__main__':

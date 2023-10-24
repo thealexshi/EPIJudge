f2e7f98b6797ba785edd6bdae7a3849c5cf04b2d
Alex Shi <thealexshi@gmail.com>
2023-10-24 16:36:49 -0700

    Sunset view brute force

--- epi_judge_python/sunset_view.py
+++ epi_judge_python/sunset_view.py
@@ -4,8 +4,32 @@ from test_framework import generic_test
 
 
 def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    EAST TO WEST ORDER
+
+    Building at position i can view sunset if:
+    - all buildings from positions 0 to i-1 have a height <= sequence[i]
+
+    Option 1: For each building, scan all previous buildings to make sure heights are less than or equal to 
+    current. Time: O(N^2). 
+    Option 2: Scan left to right, keep track of running max which you compare to each position. Time: O(N)
+
+    1 3 2
+    max = 3
+
+
+
+    """
+    max_ = float('-inf')
+    res = []
+
+    for index in range(len(sequence)-1, -1, -1):
+        height = sequence[index]
+        if height > max_:
+            res.append(index)
+        max_ = max(max_, height)
+
+    return res
 
 
 def examine_buildings_with_sunset_wrapper(sequence):

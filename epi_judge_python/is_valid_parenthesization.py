90247f0ea1acd8353abe5aa013f09ed07ff5c986
Alex Shi <thealexshi@gmail.com>
2023-10-23 22:50:40 -0700

    Valid paren

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    stack = []
+    lookup = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+
+    for c in s:
+        if c in lookup:
+            stack.append(c)
+        elif len(stack) == 0 or c != lookup[stack.pop()]:
+            return False
+
+    return len(stack) == 0
 
 
 if __name__ == '__main__':

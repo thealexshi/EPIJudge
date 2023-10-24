4d60b674f5ad4dee55a9c5107aca8b2b6348b935
Alex Shi <thealexshi@gmail.com>
2023-10-23 22:42:28 -0700

    Evaluate RPN

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
+from collections import deque
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    ex: 3, 4, +, 2, x, 1 + = (3+4)*2+1
+
+
+    """
+    stack = []
+    operators = {
+        '+': lambda y, x: y + x,
+        '-': lambda y, x: x - y,
+        '*': lambda y, x: x*y,
+        '/': lambda y, x: x // y
+    }
+
+    for token in expression.split(','):
+        if token in operators:
+            stack.append(operators[token](stack.pop(), stack.pop()))
+        else:
+            stack.append(int(token))
+
+    return stack.pop()
 
 
 if __name__ == '__main__':

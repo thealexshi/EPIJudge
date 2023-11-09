8f00de3c4724cb9b25f140c70ee1be7e3fe45d49
Alex Shi <thealexshi@gmail.com>
2023-11-08 16:16:07 -0800

    Buy and sell stock

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,26 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    """
+    Need to find biggest difference between a price and then highest price after.
+
+    If you buy stock at index i, the max possible profit from that purchase is 
+    (the highest value in prices[i+1:] - price[i]). 
+    If you sell stock at index i, max profit is price[i] - min(prices[:i])
+
+    Brute force: for every single price, calculate the max possible profit, return the max of those. O(N^2)
+
+    Keep track of the min so far, max_profit, apply formula 2 in a single pass
+    """
+
+    min_ = prices[0]
+    max_profit = 0
+
+    for price in prices:
+        max_profit = max(max_profit, price - min_)
+        min_ = min(min_, price)
+
+    return max_profit
 
 
 if __name__ == '__main__':

ff7351f6cdad2278be0d7b1790b182e8ec8d4d16
Alex Shi <thealexshi@gmail.com>
2023-11-12 20:51:47 -0800

    spreadsheet encoding

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def ss_decode_col_id(col: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    A = 1, B = 2, Z = 26, AA = 27, AB = 28
+
+    - Go through string back to front
+    """
+    res = 0
+    place = 0
+    for c in reversed(col):
+       res += (26**place)*(ord(c) - ord('A') + 1)
+       place += 1
+
+    return res
 
 
 if __name__ == '__main__':
